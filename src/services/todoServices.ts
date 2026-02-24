56e556710f79c66e4ed4305fe07c9fe317b94eb9
Gaius Okoase Oreoluwa <gaiusonosetale.com>
2026-02-24 20:13:09 +0100

    feat: implement CRUD service operations for todo

--- src/services/todoServices.ts
+++ src/services/todoServices.ts
@@ -11,10 +11,66 @@ export const createTodo = async (userId: string, todoData: any) => {
         priority,
         deadline
     })
-    todo.save()
 
     return {
         todo
     }
 }
 
+// Get Single Todo function
+export const getSingleTodo = async (userId: string, todoId: string) => {
+
+    const todo = await Todo.findOne({ _id: todoId, userId: userId });
+
+    return { todo }
+
+}
+
+// Get all Todo funtion
+export const getAllTodo = async (userId: string) => {
+
+    const todos = (await Todo.find({userId}).sort({createdAt: -1}));
+    
+    return { todos }
+}
+
+// Update Todo function
+export const updateTodo = async (userId: string, todoId: string, todoData: any) => {
+
+    const updatedTodo = await Todo.findOneAndUpdate(
+        { _id: todoId, userId},
+        todoData,
+        {new: true, runValidators: true}
+    )
+
+    return {
+        updatedTodo
+    }
+}
+
+// Delete Todo function
+export const deleteSingleTodo = async (userId: string, todoId: string) => {
+
+    const deletedTodo = await Todo.findOneAndDelete({_id: todoId, userId: userId});
+
+    return { deletedTodo }
+}
+
+// Delete Multiple Todo
+export const deleteMultipleTodo = async (userId: string, todoIds: string[]) => {
+
+    const deletedTodos = await Todo.deleteMany({
+        userId: userId,
+        _id: {$in: todoIds}
+    })
+    
+    return { deletedTodos}
+}
+
+// Delete All Todo
+export const deleteAllTodo = async (userId: string) => {
+
+    const deletedTodos = await Todo.deleteMany({userId})
+
+    return { deletedTodos }
+}
