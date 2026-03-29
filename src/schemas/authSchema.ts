cb7e9a7a59e7e64c6643568191bbb3990ffe65ed
Gaius Okoase Oreoluwa <gaiusonosetale.com>
2026-03-29 07:47:47 +0100

    feat: update authentication schemas to use detailed error messages and rename for consistency

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -1,13 +1,25 @@
 import zod from 'zod';
 
-export const RegisterAuth = zod.object({
-    firstName: zod.string('Please fill in your first name.').trim(),
-    lastName: zod.string('Please fill in your last name.').trim(),
-    email: zod.email('Please enter your email.').trim(),
-    password: zod.string('Please enter your password').min(8, 'Password must be at least 8 characters')
-})
+export const RegisterSchema = zod.object({
+    firstName: zod.string({
+        error: (iss) => iss.input === undefined ? `Please provide your first name.` : `First name should be a string.`
+    }).trim(),
+    lastName: zod.string({
+        error: (iss) => iss.input === undefined ? `Please provide your last name.` : `First name should be a string.`
+    }).trim(),
+    email: zod.email({
+        error: (iss) => iss.input === undefined ? `Please provide your email.` : `Please provide a valid email address.`
+    }).trim(),
+    password: zod.string({
+        error: (iss) => iss.input === undefined ? `Please provide a password.` : `Password should be a string.`
+    }).min(8, 'Password must be at least 8 characters.')
+});
 
-export const LoginAuth = zod.object({
-    email: zod.email('Please enter your email.').trim(),
-    password: zod.string('Please enter your password').min(8, 'Password must be at least 8 characters')
-})
+export const LoginSchema = zod.object({
+    email: zod.email({
+        error: (iss) => iss.input === undefined ? `Please provide your email.` : `Please provide a valid email address.`
+    }).trim(),
+    password: zod.string({
+        error: (iss) => iss.input === undefined ? `Please provide a password.` : `Password should be a string.`
+    }).min(8, 'Password must be at least 8 characters.')
+});
