3fc8dcffb2e2499ae4c9033cfe06bdaa79c3f12d
Gaius Okoase Oreoluwa <gaiusonosetale.com>
2026-03-29 15:46:59 +0100

    feat: add registerValidator middleware for user registration validation

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { registerUserController, loginUserController } from "../controllers/authController.js";
+import { registerValidator } from "../middleware/validate.js";
 
 const router = express.Router();
 
-router.post('/register', registerUserController);
+router.post('/register', registerValidator, registerUserController);
 router.post('/login', loginUserController);
 
 export default router;
