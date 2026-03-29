e266a5d3f39817cdd830f188507e83c26f9faf51
Gaius Okoase Oreoluwa <gaiusonosetale.com>
2026-03-29 15:37:29 +0100

    Revert "feat: add registerValidator middleware for user registration validation"
    This reverts commit e13040eda82250230256d3e51606f29da71f13e3.

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,10 +1,9 @@
 import express from "express";
 import { registerUserController, loginUserController } from "../controllers/authController.js";
-import { registerValidator } from "../middleware/validate.js";
 
 const router = express.Router();
 
-router.post('/register', registerValidator, registerUserController);
+router.post('/register', registerUserController);
 router.post('/login', loginUserController);
 
 export default router;
